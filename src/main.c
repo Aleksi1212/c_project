e742c132615c5430d211238f2efb87719683b38b
Aleksi1212 <aleksinoro@gmail.com>
2025-03-08 16:28:35 +0200

    basic structure

--- src/main.c
+++ src/main.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
 
-int main(void)
+#include "../include/commands.h"
+#include "../include/utils.h"
+
+int main(int argc, char *argv[])
 {
-    printf("hello from project\n");
+    if (argc <= 1) {
+        printf("No arguments were provided. Available commands:\n");
+        print_help();
+        return 1;
+    }
+
+    switch (get_command_code(argv[1]))
+    {
+    case 0:
+        /* code */
+        break;
+    case 1:
+        /* code */
+        break;
+    case 2:
+        /* code */
+        break;
+    case 3:
+        /* code */
+        break;
+    case 4:
+        print_help();
+        break;
+    
+    default:
+        printf("Invalid command %s. Available commands:\n", argv[1]);
+        print_help();
+        return 1;
+    }
 
     return 0;
 }
