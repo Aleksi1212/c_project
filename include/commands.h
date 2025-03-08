a5573b983a144d5ba22fc2bfdf4787e13cd235aa
Aleksi1212 <aleksinoro@gmail.com>
2025-03-09 00:29:12 +0200

    headers for commands.c

--- include/commands.h
+++ include/commands.h
@@ -1,6 +1,13 @@
+#include "structs.h"
+
 #ifndef COMMANDS_H
 #define COMMANDS_H
 
-void print_help();
+void add(const char *repos_path, const Repo *repos, int *repos_count, const char *alias, const char *link);
+void get(const Repo *repos, int *repos_count, const char *alias);
+void list(const Repo *repos, int *repos_count);
+void delete(const char *repos_path, Repo *repos, int *repos_count, const char *alias);
+
+void help();
 
 #endif
