3e74ad5d7d3569a9359c40da16674770299fbc3a
Aleksi1212 <aleksinoro@gmail.com>
2025-03-09 00:27:24 +0200

    show -> get, and function to remove new line from string

--- src/utils.c
+++ src/utils.c
@@ -5,7 +5,7 @@ int get_command_code(const char *command)
 {
     if (strcmp(command, "--add") == 0 || strcmp(command, "-a") == 0)
         return 0;
-    if (strcmp(command, "--show") == 0 || strcmp(command, "-s") == 0)
+    if (strcmp(command, "--get") == 0 || strcmp(command, "-g") == 0)
         return 1;
     if (strcmp(command, "--list") == 0 || strcmp(command, "-l") == 0)
         return 2;
@@ -15,4 +15,13 @@ int get_command_code(const char *command)
         return 4;
 
     return -1;
+}
+
+void remove_new_line(char *str)
+{
+    size_t len = strlen(str);
+    if (len > 0 && str[len - 1] == '\n')
+    {
+        str[len - 1] = '\0';
+    }
 }
