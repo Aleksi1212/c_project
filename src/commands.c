c6a3e44c1d044009487d84efef737ac779832125
Aleksi1212 <aleksinoro@gmail.com>
2025-03-09 21:55:10 +0200

    function and info for populate command

--- src/commands.c
+++ src/commands.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
 
 #include "../include/commands.h"
 #include "../include/structs.h"
 #include "../include/repos.h"
-
-#define COMMAND_LIST_ARR_SIZE 5
+#include "../include/utils.h"
+#include "../include/constants.h"
 
 void add(const char *repos_path, const Repo *repos, int *repos_count, const char *alias, const char *link)
 {
@@ -91,6 +93,29 @@ void delete(const char *repos_path, Repo *repos, int *repos_count, const char *a
     printf("File is empty\n");
 }
 
+void populate(const char* repos_path, const Repo *repos, int *repos_count, int count, size_t key_size, size_t value_size)
+{
+    if (key_size > MAX_KEY_SIZE || value_size > MAX_VALUE_SIZE) {
+        printf("Key or value size too large.\n");
+        return;
+    }
+
+    srand(time(NULL));
+
+    for (int i = 0; i < count; i++) {
+        char *rand_alias = malloc(key_size);
+        char *rand_link = malloc(value_size);
+
+        rand_str(rand_alias, key_size);
+        rand_str(rand_link, value_size);
+
+        add(repos_path, repos, repos_count, rand_alias, rand_link);
+
+        free(rand_alias);
+        free(rand_link);
+    }
+}
+
 void help()
 {
     const int INDENTATION = 5;
@@ -113,9 +138,13 @@ void help()
     command_list_arr[3].info = "deletes the given alias";
     command_list_arr[3].indent = strlen(command_list_arr[3].info) + INDENTATION;
 
-    command_list_arr[4].name = "--help | -h";
-    command_list_arr[4].info = "lists all available commands";
+    command_list_arr[4].name = "--populate <count> <key_size> <value_size> | -p <count> <key_size> <value_size>";
+    command_list_arr[4].info = "populate with data <count> amount of data with key being of <key_size> and value of <value_size>";
     command_list_arr[4].indent = strlen(command_list_arr[4].info) + INDENTATION;
+
+    command_list_arr[5].name = "--help | -h";
+    command_list_arr[5].info = "lists all available commands";
+    command_list_arr[5].indent = strlen(command_list_arr[5].info) + INDENTATION;
 
     for (int i = 0; i < COMMAND_LIST_ARR_SIZE; i++)
     {
