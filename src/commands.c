ed051a0cd23472b00a8fe9bd29cafc620c3e2e6c
Aleksi1212 <aleksinoro@gmail.com>
2025-03-11 17:05:41 +0200

    cleanup

--- src/commands.c
+++ src/commands.c
@@ -31,18 +31,8 @@ void add(char *repos_path, const Repo *repos, int *repos_count, const char *alia
     char path_copy[2048];
     strcpy(path_copy, repos_path);
 
-    char *token = strtok(path_copy, "/");
-    struct stat sb;
-    while (token)
-    {
-        if (is_file_extension(token, ".txt") != 1) {
-            if (stat(token, &sb) != 0) {
-                mkdir(token, 0700);
-            }
-        }
-        token = strtok(NULL, "/");
-    }
-    
+    remove_file_from_dir_string(path_copy);
+    make_dir(path_copy);
 
     FILE *file = fopen(repos_path, "a+");
     if (file == NULL) {
@@ -53,7 +43,7 @@ void add(char *repos_path, const Repo *repos, int *repos_count, const char *alia
     fprintf(file, "%s;%s\n", alias, link);
     fclose(file);
 
-    printf("Succesfully added alias\n");
+    printf("OK\n");
 }
 
 void get(const Repo *repos, int *repos_count, const char *alias)
@@ -89,22 +79,26 @@ void list(const Repo *repos, int *repos_count)
 
 void delete(const char *repos_path, Repo *repos, int *repos_count, const char *alias)
 {
-    if (alias == NULL) {
-        remove(repos_path);
-        printf("Succesfully removed\n");
-        return;
-    }
-
     if (repos != NULL) {
+        if (alias == NULL) {
+            remove(repos_path);
+            printf("OK\n");
+            return;
+        }
+
         FILE *file = fopen(repos_path, "w");
+        if (file == NULL) {
+            printf("Error opening file\n");
+            return;
+        }
 
         for (int i = 0; i < *repos_count; i++) {
             if (strcmp(repos[i].alias, alias) != 0) {
                 fprintf(file, "%s;%s\n", repos[i].alias, repos[i].link);
             }
         }
-
-        printf("Successfully removed.\n");
+        fclose(file);
+        printf("OK\n");
         return;
     }
     printf("File is empty\n");
@@ -113,7 +107,7 @@ void delete(const char *repos_path, Repo *repos, int *repos_count, const char *a
 void populate(char* repos_path, const Repo *repos, int *repos_count, int count, size_t key_size, size_t value_size)
 {
     if (key_size > MAX_KEY_SIZE || value_size > MAX_VALUE_SIZE) {
-        printf("Key or value size too large.\n");
+        printf("Key or value size too large. MAX key size: %d, MAX value size: %d\n", MAX_KEY_SIZE, MAX_VALUE_SIZE);
         return;
     }
 
@@ -135,33 +129,31 @@ void populate(char* repos_path, const Repo *repos, int *repos_count, int count, 
 
 void help()
 {
-    const int INDENTATION = 5;
-
     Command_list command_list_arr[COMMAND_LIST_ARR_SIZE];
 
     command_list_arr[0].name = "--add <alias> <repository_link> | -a <alias> <repository_link>";
     command_list_arr[0].info = "adds a new repository";
-    command_list_arr[0].indent = strlen(command_list_arr[0].info) + INDENTATION;
+    command_list_arr[0].indent = strlen(command_list_arr[0].info) + INFO_INDENTATION;
 
     command_list_arr[1].name = "--get <alias> | -g <alias> | --get | -g";
     command_list_arr[1].info = "get the repository link of a given alias or get all links";
-    command_list_arr[1].indent = strlen(command_list_arr[1].info) + INDENTATION;
+    command_list_arr[1].indent = strlen(command_list_arr[1].info) + INFO_INDENTATION;
 
     command_list_arr[2].name = "--list | -l";
     command_list_arr[2].info = "lists all aliases";
-    command_list_arr[2].indent = strlen(command_list_arr[2].info) + INDENTATION;
+    command_list_arr[2].indent = strlen(command_list_arr[2].info) + INFO_INDENTATION;
 
     command_list_arr[3].name = "--delete <alias> | -d <alias> | --delete | -d";
     command_list_arr[3].info = "deletes the given alias or no alias to delete all";
-    command_list_arr[3].indent = strlen(command_list_arr[3].info) + INDENTATION;
+    command_list_arr[3].indent = strlen(command_list_arr[3].info) + INFO_INDENTATION;
 
     command_list_arr[4].name = "--populate <count> <key_size> <value_size> | -p <count> <key_size> <value_size>";
     command_list_arr[4].info = "populate with data <count> amount of data with key being of <key_size> and value of <value_size>";
-    command_list_arr[4].indent = strlen(command_list_arr[4].info) + INDENTATION;
+    command_list_arr[4].indent = strlen(command_list_arr[4].info) + INFO_INDENTATION;
 
     command_list_arr[5].name = "--help | -h";
     command_list_arr[5].info = "lists all available commands";
-    command_list_arr[5].indent = strlen(command_list_arr[5].info) + INDENTATION;
+    command_list_arr[5].indent = strlen(command_list_arr[5].info) + INFO_INDENTATION;
 
     for (int i = 0; i < COMMAND_LIST_ARR_SIZE; i++)
     {
